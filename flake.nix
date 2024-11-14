19b75aa3d6a26a36373e5976a358a9da45ff6f02
Tristan Cacqueray <tdecacqu@redhat.com>
2024-11-14 15:59:50 -0500

    Add release workflow

--- flake.nix
+++ flake.nix
@@ -14,6 +14,9 @@
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python311.withPackages
           (ps: [ ps.openstacksdk ps.prometheus-client ps.pyyaml ]);
+
+        info = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+
         container-name = "ghcr.io/tristancacqueray/zuul-capacity";
         container = pkgs.dockerTools.streamLayeredImage {
           name = container-name;
@@ -27,8 +30,26 @@
           };
         };
 
+        publish-container-release =
+          pkgs.writeShellScriptBin "container-release" ''
+            set -e
+            export PATH=$PATH:${pkgs.gzip}/bin:${pkgs.skopeo}/bin
+            IMAGE="docker://${container-name}"
+
+            echo "Logging to registry..."
+            echo $GH_TOKEN | skopeo login --username $GH_USERNAME --password-stdin ghcr.io
+
+            echo "Building and publishing the image..."
+            ${container} | gzip --fast | skopeo copy docker-archive:/dev/stdin $IMAGE:${info.project.version}
+
+            echo "Tagging latest"
+            skopeo copy $IMAGE:${info.project.version} $IMAGE:latest
+          '';
+
       in {
         packages.container = container;
+        apps.publish-container-release =
+          flake-utils.lib.mkApp { drv = publish-container-release; };
         devShell = pkgs.mkShell { buildInputs = with pkgs; [ uv ]; };
       });
 }
